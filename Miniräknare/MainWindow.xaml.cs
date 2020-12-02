ad8858b1fcf0448a235e05fd4f326585350a76af
judenleif <54800741+judenleif@users.noreply.github.com>
2020-12-02 08:56:46 +0100

    a

--- Miniräknare/MainWindow.xaml.cs
+++ Miniräknare/MainWindow.xaml.cs
@@ -32,8 +32,112 @@ namespace Miniräknare
         }
         private void Button_Clear(object sender, RoutedEventArgs e)
         {
-            
+            Numbers.Text = string.Empty;
+        }
+        private void Button_Calculate(object sender, RoutedEventArgs e)
+        {
+
         }
 
+        private void Button_Addition(object sender, RoutedEventArgs e)
+        {
+            tal1 = input;
+            operation = '+';
+            input = string.Empty;
+        }
+        private void Button_subtraction(object sender, RoutedEventArgs e)
+        {
+            tal1 = input;
+            operation = '-';
+            input = string.Empty;
+
+        }
+        private void Button_Division(object sender, RoutedEventArgs e)
+        {
+            tal1 = input;
+            operation = '/';
+            input = string.Empty;
+        }
+        private void Button_Muliplication(object sender, RoutedEventArgs e)
+        {
+            tal1 = input;
+            operation = '*';
+            input = string.Empty;
+
+        }
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            input += '0';
+        }
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            input += '1';
+        }
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            input += '2';
+        }
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            input += '3';
+        }
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            input += '4';
+        }
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            input += '5';
+        }
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            input += '6';
+        }
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            input += '7';
+        }
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            input += '8';
+        }
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            input += '1';
+
+        }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
